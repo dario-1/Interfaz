43f24521c8d3833d3fd2a13aabd04bc7c1e4215c
dario-1 <renancitosalazar@gmail.com>
2023-09-18 14:26:27 -0500

    menus desplegales

--- usuario/gui_app.py
+++ usuario/gui_app.py
@@ -9,6 +9,8 @@ def barra_menu(root):
     menu_inicio.add_command(label='Analisis en Equipo1')
     menu_inicio.add_command(label='Analisis en Equipo2')
     menu_inicio.add_command(label='Salir',command=root.destroy)
+    
+      
 
 
 
@@ -18,12 +20,35 @@ class Frame(tk.Frame):
         self.root=root
         self.pack()
         self.config(bg='green')
-        self.seleccion_metodo()
+        self.etiquetas()
+        self.options()
     
-    def seleccion_metodo(self):
-        self.label_nombre=tk.Label(self,text='Seleccion de Metodo')
-        self.label_nombre.config(font=('Arial',12,'bold'))
-        self.label_nombre.grid(row=0,column=0)
+    def etiquetas(self):
+        self.label_metodo=tk.Label(self,text='Seleccion de Metodo')
+        self.label_metodo.config(font=('Arial',12,'bold'))
+        self.label_metodo.grid(row=0,column=0,padx=10,pady=10)
+        
+        self.label_proceso=tk.Label(self,text='Seleccion de Proceso')
+        self.label_proceso.config(font=('Arial',12,'bold'))
+        self.label_proceso.grid(row=2,column=0,padx=20,pady=20)
+    
+    def options(self):
+        style = tk.ttk.Style()
+        style.configure('Menu.TButton', font=('Arial', 12, 'bold'))
+        self.met = tk.StringVar(self.root)
+        self.met.set("Seleccione un metodo")
+        submenu = tk.OptionMenu(self, self.met, "Metodo 1", "Metodo 2")
+        submenu.grid(row=1, column=0, padx=20, pady=20)
+        self.proces = tk.StringVar(self.root)
+        self.proces.set("Seleccione un Proceso")
+        submenu1 = tk.OptionMenu(self, self.proces, "Proceso 1", "Proceso 2",
+                                 "Proceso 3", "Proceso 4","Proceso 5", "Proceso 6",
+                                 "Proceso 7", "Proceso 8","Proceso 9",
+                                "Proceso 10","Proceso 11", "Proceso 12")
+        submenu1.grid(row=3, column=0, padx=20, pady=20)
+        
+        
+       
         
 
 
